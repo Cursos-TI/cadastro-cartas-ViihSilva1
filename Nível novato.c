f6a2ce5bd15b7edaaeafe8af7772220c09bd7b77
ViihSilva1 <vitorialcve@gmail.com>
2026-04-07 01:52:31 +0000

    Operações matematicas

---
+++ Nível novato.c
@@ -0,0 +1,50 @@
+#include <stdio.h> 
+
+int main (){
+    /*
+    Soma (+)
+    Subtração (-)
+    Multiplicação (*)
+    Divisão (/)
+    */
+
+int numero1, numero2;
+int soma, subtracao, multiplicacao, divisao;
+   
+    printf("Entre com o numero 1: \n");
+    scanf("%d", &numero1);
+    printf("Entre com o numero 2: \n");
+    scanf("%d", &numero2);
+    
+
+
+    //operação soma
+    soma = numero1 + numero2;
+    
+    //operação subtracao
+    subtracao = numero1 - numero2;
+    
+    //operacao multiplicacao
+    multiplicacao = numero1 * numero2;
+    
+    //operacao divisao
+    divisao = numero1 / numero2;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
