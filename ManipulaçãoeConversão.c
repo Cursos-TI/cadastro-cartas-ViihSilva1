b0425f0cf0df70e9c0126d12e7d9630b58ce998b
ViihSilva1 <vitorialcve@gmail.com>
2026-04-08 01:27:57 +0000

    Conversão

---
+++ ManipulaçãoeConversão.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+// Daqui para baixo é variáveis inteiras
+int main (){
+    //int a = 10;
+    //int b = 3;
+    //int soma = a + b;
+    //int diferenca = a - b;
+    //int produto = a * b;
+    //int quociente = a / b; // Note que a divisão de inteiros resulta em um número inteiro
+
+    //printf("Soma: %d\n", soma);
+    //printf("Diferenca: %d\n", diferenca);
+    //printf("Produto: %d\n", produto);
+    //printf("Quociente: %d\n", quociente);
+
+// Daqui para baixo é variáveis de ponto flutuante
+    float x = 5.5;
+    float y = 2.2;
+    float soma = x + y;
+    float diferenca = x - y;
+    float produto = x * y;
+    float quociente = x / y; // Divisão de ponto flutuante
+
+    printf("Soma: %.2f\n", soma);
+    printf("Diferenca: %.2f\n", diferenca);
+    printf("Produto: %.2f\n", produto);
+    printf("Quociente: %.2f\n", quociente);
+
+
+
+
+
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
